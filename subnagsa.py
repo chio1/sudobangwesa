6adaedee4d21419fb9dfaab99c8de3e15ee7bf92
chio1 <36753740+chio1@users.noreply.github.com>
2019-07-08 19:02:41 +0900

    Add files via upload

---
+++ subnagsa.py
@@ -0,0 +1,20 @@
+import discord
+import asyncio
+import openpyxl
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('온')
+    print('------')
+    game = discord.Game("!명령어 l 제작중")
+    await client.change_presence(status=discord.Status.online , activity=game)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!명령어"):
+        await message.channel.send(embed=discord.Embed(title="제작중", description= "\n\n\n차후추가예정", color=0x00ff00))
+
+
